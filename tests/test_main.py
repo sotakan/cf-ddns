07e68ae6817ace9d87d90b5590ddb88f669a4966
Sota Kaneko <me@sotakan.com>
2022-04-22 01:20:22 -0700

    Feat/getconfig (#2)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
 import unittest
 import main
+import os
+
+# For all tests to work set the environment variable CFDDNS_API_TOKEN via your profile or .env file
 
 class TestMain(unittest.TestCase):
     @classmethod
@@ -14,3 +17,9 @@ class TestMain(unittest.TestCase):
     
     def test_getpreviousip(self):
         self.assertEqual(self.ip.getpreviousip(), "1.1.1.1")
+
+    def test_getconfig(self):
+        self.assertIsInstance(main.getconfig(), list)
+
+    def test_gettoken(self):
+        self.assertNotEqual(main.gettoken(), None)
